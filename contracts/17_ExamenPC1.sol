2a10ea54d87c3d1770b270da485e577013f0aab4
CesarArm <cesarpinillos18@gmail.com>
2026-05-16 20:33:20 -0500

    Parte 5

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -12,7 +12,10 @@ contract Hospital251938 {
         bool estado;
     }
 
-    Paciente[] public registros;
+    mapping(uint256 => Paciente) public registros;
+    uint256[] private listaIds;
+    
+    uint256 public cantidad;
     address public dirContrato = 0x9396B453Fad71816cA9f152Ae785276a1D578492;
 
     modifier mostrarConsola() {
@@ -26,35 +29,36 @@ contract Hospital251938 {
 
     function agregarElemento(uint256 _id, string memory _nombre, uint256 _edad, bool _estado) public mostrarConsola {
         require(bytes(_nombre).length > 0, "El nombre no puede estar vacio");
+        require(registros[_id].id == 0, "El ID ya esta registrado");
 
-        for (uint256 i = 0; i < registros.length; i++) {
-            require(registros[i].id != _id, "El ID ya esta registrado");
-        }
-
-        registros.push(Paciente(_id, _nombre, _edad, _estado));
+        registros[_id] = Paciente(_id, _nombre, _edad, _estado);
+        listaIds.push(_id);
+        cantidad++;
     }
 
     function contarElementos() public view mostrarConsola returns (uint256) {
-        return registros.length;
+        return cantidad;
     }
 
-    function inactivarElemento(uint256 _posicion) public mostrarConsola {
-        require(_posicion < registros.length, "Posicion fuera de rango");
-        registros[_posicion].estado = false;
+    function inactivarElemento(uint256 _id) public mostrarConsola {
+        require(registros[_id].id != 0, "El ID no existe");
+        registros[_id].estado = false;
     }
 
     function pintarElementosActivos() public view mostrarConsola {
-        for (uint256 i = 0; i < registros.length; i++) {
-            if (registros[i].estado == true) {
-                console.log("Mascota activa:", registros[i].id, registros[i].nombre);
+        for (uint256 i = 0; i < listaIds.length; i++) {
+            uint256 idActual = listaIds[i];
+            if (registros[idActual].estado == true) {
+                console.log("Mascota activa:", registros[idActual].id, registros[idActual].nombre);
             }
         }
     }
 
     function pintarElementosImpares() public view mostrarConsola {
-        for (uint256 i = 0; i < registros.length; i++) {
-            if (registros[i].id % 2 != 0) {
-                console.log("Mascota ID impar:", registros[i].id, registros[i].nombre);
+        for (uint256 i = 0; i < listaIds.length; i++) {
+            uint256 idActual = listaIds[i];
+            if (registros[idActual].id % 2 != 0) {
+                console.log("Mascota ID impar:", registros[idActual].id, registros[idActual].nombre);
             }
         }
     }
