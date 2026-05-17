c36408a6e3b2b3e7b02db49bac4ab2c8dac42844
CesarArm <cesarpinillos18@gmail.com>
2026-05-16 20:14:05 -0500

    Parte 3

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -9,17 +9,29 @@ contract Hospital251938 {
         uint256 id;
         string nombre;
         uint256 edad;
+        bool estado;
     }
 
     Paciente[] public registros;
     address public dirContrato = 0x9396B453Fad71816cA9f152Ae785276a1D578492;
 
+    modifier mostrarConsola() {
+        console.log("Ejecutado por: 251938 - Cesar Armando Pinillos Gomez");
+        _;
+    }
+
     constructor() {
         console.log("Ejecutado por: 251938 - Cesar Armando Pinillos Gomez");
     }
 
-    function agregarElemento(uint256 _id, string memory _nombre, uint256 _edad) public {
-        registros.push(Paciente(_id, _nombre, _edad));
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _edad, bool _estado) public mostrarConsola {
+        require(bytes(_nombre).length > 0, "El nombre no puede estar vacio");
+
+        for (uint256 i = 0; i < registros.length; i++) {
+            require(registros[i].id != _id, "El ID ya esta registrado");
+        }
+
+        registros.push(Paciente(_id, _nombre, _edad, _estado));
     }
 
     function contarElementos() public view returns (uint256) {
